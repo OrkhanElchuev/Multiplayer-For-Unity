1da85bad57d1cd14527ec4a0a10c869d70ffee34
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-26 21:12:35 +0100

    Connected to the EU server

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Connects to Photon master servers
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // Display with which region we established connection
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server.");
+    }
+}
