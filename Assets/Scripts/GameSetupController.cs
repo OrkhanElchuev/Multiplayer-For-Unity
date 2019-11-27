6a3f78bda38a06a22b489872fc7e515fd9a902a5
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-27 01:38:58 +0100

    New scene added, GameSetup script created, demo player added

---
+++ Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+
+// This script will be added to any multiplayer scene
+public class GameSetupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create a player object with a network for each player
+        // who load into the multiplayer scene
+        CreatePlayer();
+    }
+
+    // Handle player creation process
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating a player");
+        // Passing string location to the prefab that needs to be instantiated
+        // Position and rotation are defined for instantiation
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"),
+        Vector3.zero, Quaternion.identity);
+    }
+}
