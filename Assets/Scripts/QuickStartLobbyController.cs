59270fe6842852e6ba2599fa99acb324c8a0f233
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-27 01:10:11 +0100

    Lobby controller script created

--- Assets/Scripts/QuickStartLobbyController.cs
+++ Assets/Scripts/QuickStartLobbyController.cs
@@ -36,9 +36,46 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
         Debug.Log("Quick Start");
     }
 
+    // Handle the Quick Cancel Button functionality
+    public void QuickCancel()
+    {
+        // When pressed disable cancel button and enable start button
+        quickCancelButton.SetActive(false);
+        quickStartButton.SetActive(true);
+        // Leave the room
+        PhotonNetwork.LeaveRoom();
+    }
+
     // In case of failing to join a room
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("Failed to join a room");
+    }
+
+    // Try to create own room
+    void CreateRoom()
+    {
+        Debug.Log("Creating room");
+        // Random number for a room name
+        int randomRoomNumber = Random.Range(0, 10000);
+        // Setting room configurations
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = (byte)roomSize
+        };
+        // Attempt to create a new room
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOptions);
+        Debug.Log(randomRoomNumber);
+    }
+
+    // In case of failing to create a new room 
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create a room, try again");
+        // Try creating room again with a different random number because
+        // Fail might occur if the room with generated number already exists
+        CreateRoom();
     }
 }
